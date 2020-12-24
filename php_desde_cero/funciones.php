d67c94909351df8170a25c043e9d82dafd663d36
David Cordoba <davbaster@gmail.com>
2020-12-24 11:10:49 -0600

    completado funciones.php

--- php_desde_cero/funciones.php
+++ php_desde_cero/funciones.php
@@ -1,3 +1,26 @@
 <?php
 
+// // funciones anonimas
+// $despedida = function(){
+// 	echo "Ultima clase de php desde cero, gracias David por el esfuerzo.";
+// };
+
+// $despedida();
+
+// FUNCION ANONIMA CLOSURE
+function finalizarCurso(Closure $curso, $nombre){
+	return $curso($nombre);
+};
+
+$php = function($nombre){
+	return "<p>Gracias ".$nombre." por finalizar el curso de PHP.";
+};
+
+$laravel = function($nombre) {
+	return "<p>Gracias ".$nombre." por finalizar el curso de Laravel";
+};
+
+echo finalizarCurso($php, 'David');
+echo finalizarCurso($laravel, 'David');
+
 ?>
