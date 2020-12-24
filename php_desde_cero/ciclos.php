079b8fdce8b548ea845d399a1126bff0dc0b593f
David Cordoba <davbaster@gmail.com>
2020-12-24 10:28:39 -0600

    actualizado cilos.php y creado funciones.php

---
+++ php_desde_cero/ciclos.php
@@ -0,0 +1,18 @@
+<?php
+
+// for each en arreglo indexado.
+$redesSocialesI = array('facebook','reddit', 'instagram', 'whatsapp');
+
+foreach ($redesSocialesI as $valor ) {
+	echo $valor."<br />";
+}
+
+// for each en arreglo asociativo.
+// $redesSociales = array('FB' => 'facebook', 'red' => 'reddit', 'insta' => 'instagram');
+
+// foreach ($redesSociales as $key => $value) {
+// 	echo $value."<br />";
+// }
+
+
+?>
