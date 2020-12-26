7ecd158c8e08e9b3402159ce72a38022bb28c718
David Cordoba <davbaster@gmail.com>
2020-12-26 14:20:10 -0600

    creado carpeta paginarecurso

---
+++ php_desde_cero/paginacursos/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Cursos de programacion</title>
+</head>
+<body>
+	<h1>Cursos de programacion</h1>
+
+	<?php
+
+		define('INC', '/includes/');
+		require_once __DIR__ . INC .'Curso.inc.php';
+
+
+
+
+
+		?>
+	
+</body>
+</html>
